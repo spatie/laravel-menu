503ccdd590a9c1ee03d4a21453fe5a8c824ef6eb
Richard Hülsberg <richard.huelsberg@ort-online.net>
2021-02-10 07:52:50 +0100

    Implement Issue 108 test

--- tests/Issue108Test.php
+++ tests/Issue108Test.php
@@ -9,7 +9,10 @@ use Spatie\Menu\Laravel\Link;
 class Issue108Test extends TestCase
 {
 
-    /** @test */
+    /**
+     * @test
+     * It works in 2.9, but not in 2.10 from spatie/menu
+     */
     public function renderWithSpatieMenu2_10_2()
     {
         Menu::macro('admin', function () {
